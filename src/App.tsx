909786142d1c8f1c462cdabe301cdaab938ef258
Rick Ryan Alinsangao <alinsangao.rickryan@gmail.com>
2023-12-12 22:53:50 +0800

    redesign

--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,149 @@
 function App() {
   return (
-    <div className="h-screen w-screen overflow-y-auto flex flex-col bg-[url('https://images.unsplash.com/photo-1553877522-43269d4ea984?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
-      <div className="h-fit md:h-1/2 my-10 flex md:flex-col flex-col justify-center items-center gap-0 w-full">
-        <h1 className="font-sans font-semibold text-5xl md:text-8xl text-[#2b323f]">
+    <div className="font-sans h-screen w-screen flex-wrap overflow-y-auto flex flex-row bg-[url('https://images.unsplash.com/photo-1553877522-43269d4ea984?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
+      <div className="h-1/4 md:h-full flex md:flex-col flex-col justify-center items-center gap-0 w-full">
+        <h1 className="font-sans font-semibold text-7xl md:text-8xl text-primary">
           PROJECT: VA;
         </h1>
-        <h2 className="font-sans font-semibold text-xs md:text-xl text-[#2b323f]">
+        <h2 className="font-sans font-semibold text-lg md:text-xl text-primary">
           Simplifying Life, Maximizing Productivity.
         </h2>
       </div>
-      <div className="text-gray-400 h-fit md:h-1/2 flex flex-col md:flex-row gap-4 font-sans px-5 justify-center mb-5">
-        <div className="hover:scale-125 animate duration-500 h-[300px] w-full md:w-1/4 rounded-md shadow-xl bg-cover bg-[url('https://images.unsplash.com/photo-1606836559739-7b1d9fbf8a6e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
-          <div className="rounded-md h-full flex justify-center flex-col items-center backdrop-blur-sm bg-[#2b323f]/80">
-            <div className="text-center text-xl md:text-3xl w-full text-white font-sans font-semibold">
-              PROJECT MANAGEMENT
-            </div>
-            <div className="indent-5 px-5 text-xs md:hidden font-sans">
-              As a Property Management Assistant, you play a vital role in
-              supporting the efficient functioning of property management
-              operations. Your responsibilities include assisting in tenant
-              communications, coordinating property maintenance tasks, and
-              ensuring accurate and up-to-date documentation. You'll collaborate
-              with Property Managers to facilitate lease agreements, track rent
-              payments, and address tenant inquiries. Attention to detail,
-              strong organizational skills, and effective communication are
-              essential in this role. Additionally, you may be involved in
-              financial tasks such as budgeting and expense tracking. Your
-              contribution as a Property Management Assistant contributes to the
-              overall success of property management by ensuring smooth
-              day-to-day operations and tenant satisfaction.
-            </div>
+      <div className="indent-5 p-10 text-base md:text-lg font-base h-auto flex md:flex-col flex-col justify-center items-center gap-4 w-full bg-[#212630] text-[#aebacb]">
+        PROJECT: VA; is a leading VA agency dedicated to providing tailored
+        support solutions for businesses worldwide. Specializing in
+        administrative tasks, customer service, and project management, our team
+        of skilled virtual assistants is committed to enhancing operational
+        efficiency and allowing our clients to focus on their core business
+        functions. Our services cover a broad spectrum, including email and
+        calendar management, data entry, social media management, and more. We
+        understand the importance of flexibility and customization, ensuring
+        that our virtual assistants seamlessly integrate into diverse business
+        environments. At PROJECT: VA;, we prioritize client satisfaction,
+        confidentiality, and reliability. Our goal is to offer a cost-effective
+        alternative to traditional in-house support, allowing businesses of all
+        sizes to benefit from professional assistance without the overhead
+        costs. By leveraging the latest technology and a team of dedicated
+        professionals, Virtual Assist Pro empowers businesses to scale,
+        innovate, and thrive in a competitive landscape. Experience the
+        convenience of a virtual support team tailored to meet your unique
+        requirements – choose PROJECT: VA; for unparalleled virtual assistance
+        expertise.
+      </div>
+      <div className="text-5xl text-center w-full font-semibold text-primary my-5">
+        SERVICES OFFERED
+      </div>
+      <div className="h-1/3 w-full gap-5 flex flex-col md:overflow-none overflow-y-auto flex-wrap text-primary p-5 justify-center items-center">
+        <div className="text-secondary bg-primary/70 md:w-1/2 lg:w-1/3 xl:w-1/4 w-full h-full rounded-lg p-5 text-center">
+          <div className="text-3xl lg:text-2xl">Property Management</div>
+          <div className="text-xl lg:text-lg indent-5 mt-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quod,
+            voluptatibus ad quo aut quam aliquid atque harum sunt similique
+            labore unde quae quis exercitationem facilis voluptas quia
+            accusantium cumque.
           </div>
         </div>
-        <div className="md:hover:scale-125 animate duration-500 h-[300px] w-full md:w-1/4 rounded-md shadow-xl bg-cover bg-[url('https://images.unsplash.com/photo-1526628953301-3e589a6a8b74?q=80&w=2006&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
-          <div className="rounded-md h-full flex justify-center flex-col items-center backdrop-blur-sm bg-[#2b323f]/80">
-            <div className="text-center text-xl md:text-3xl w-full text-white font-sans font-semibold ">
-              BOOK KEEPING
-            </div>
-            <div className="indent-5 px-5 text-xs md:hidden font-sans">
-              As a Bookkeeping Assistant, you play a key role in supporting the
-              financial health of an organization by assisting in the
-              maintenance of accurate and up-to-date financial records. Your
-              responsibilities include data entry, reconciling accounts, and
-              categorizing transactions. You collaborate closely with the
-              accounting team to ensure compliance with financial regulations
-              and timely reporting. Attention to detail, proficiency in
-              accounting software, and a basic understanding of financial
-              principles are crucial for success in this role. Your contribution
-              as a Bookkeeping Assistant helps ensure the organization's
-              financial transactions are recorded systematically, providing a
-              foundation for informed financial decision-making.
-            </div>
+        <div className="text-secondary bg-primary/70 md:w-1/2 lg:w-1/3 xl:w-1/4 w-full h-full rounded-lg p-5 text-center">
+          <div className="text-3xl lg:text-2xl">Bookkeeping</div>
+          <div className="text-xl lg:text-lg indent-5 mt-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quod,
+            voluptatibus ad quo aut quam aliquid atque harum sunt similique
+            labore unde quae quis exercitationem facilis voluptas quia
+            accusantium cumque.
           </div>
         </div>
-        <div className="md:hover:scale-125 animate duration-500 h-[300px] w-full md:w-1/4 rounded-md shadow-xl bg-cover bg-[url('https://images.unsplash.com/photo-1608699565448-9492aaf3c708?q=80&w=2151&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
-          <div className="rounded-md h-full flex justify-center flex-col items-center backdrop-blur-sm bg-[#2b323f]/80">
-            <div className="text-center text-xl md:text-3xl w-full text-white font-sans font-semibold ">
-              GRAPHIC DESIGN
-            </div>
-            <div className="indent-5 px-5 text-xs md:hidden font-sans">
-              As a Graphic Design Assistant, you contribute to the creative and
-              visual aspects of projects by providing essential support to the
-              graphic design team. Your responsibilities include assisting in
-              the design process, creating and editing visual content, and
-              ensuring brand consistency. You may collaborate on various design
-              projects, ranging from digital and print materials to social media
-              graphics. Proficiency in graphic design software, a keen eye for
-              detail, and a basic understanding of design principles are
-              essential for success in this role. Your role as a Graphic Design
-              Assistant is integral to maintaining high-quality visual standards
-              and contributing to the overall success of creative projects.
-            </div>
+        <div className="text-secondary bg-primary/70 md:w-1/2 lg:w-1/3 xl:w-1/4 w-full h-full rounded-lg p-5 text-center">
+          <div className="text-3xl lg:text-2xl">Graphic Design</div>
+          <div className="text-xl lg:text-lg indent-5 mt-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quod,
+            voluptatibus ad quo aut quam aliquid atque harum sunt similique
+            labore unde quae quis exercitationem facilis voluptas quia
+            accusantium cumque.
           </div>
         </div>
       </div>
-      <div className="md:h-auto py-5 text-white text-center font-semibold font-sans text-lg backdrop-blur-sm bg-[#2b323f]/80">
-        <div>
-          <div>Wanna hire a VA?</div>
-          <div>Send us an email at loremimpusm@gmail.com</div>
+      <div className="w-full h-1/4 flex justify-center items-center">
+        <button className="rounded-md bg-primary/90 p-5 text-white font-semibold">
+          BOOK A CONSULTATION
+        </button>
+      </div>
+      <div className="w-full h-full">
+        <div className="text-5xl text-center w-full font-semibold text-primary my-5">
+          CLIENT'S FEEDBACK
+        </div>
+        <div className="w-full h-1/3 border border-solid flex flex-col justify-between items-center gap-5 overflow-y-auto flex-wrap p-5">
+          <div className="h-full w-1/2 md:w-1/6 flex justify-center items-center flex-col bg-primary/70 rounded-md shadow-lg p-5">
+            <div className="bg-profile rounded-full h-20 w-20 bg-cover bg-right"></div>
+            <div className="italic mt-5 text-white text-sm">
+              "This is a very interesting feedback from the client"
+            </div>
+          </div>
+          <div className="h-full w-1/2 md:w-1/6 flex justify-center items-center flex-col bg-primary/70 rounded-md shadow-lg p-5">
+            <div className="bg-profile rounded-full h-20 w-20 bg-cover bg-right"></div>
+            <div className="italic mt-5 text-white text-sm">
+              "This is a very interesting feedback from the client"
+            </div>
+          </div>
+          <div className="h-full w-1/2 md:w-1/6 flex justify-center items-center flex-col bg-primary/70 rounded-md shadow-lg p-5">
+            <div className="bg-profile rounded-full h-20 w-20 bg-cover bg-right"></div>
+            <div className="italic mt-5 text-white text-sm">
+              "This is a very interesting feedback from the client"
+            </div>
+          </div>
+          <div className="h-full w-1/2 md:w-1/6 flex justify-center items-center flex-col bg-primary/70 rounded-md shadow-lg p-5">
+            <div className="bg-profile rounded-full h-20 w-20 bg-cover bg-right"></div>
+            <div className="italic mt-5 text-white text-sm">
+              "This is a very interesting feedback from the client"
+            </div>
+          </div>
+          <div className="h-full w-1/2 md:w-1/6 flex justify-center items-center flex-col bg-primary/70 rounded-md shadow-lg p-5">
+            <div className="bg-profile rounded-full h-20 w-20 bg-cover bg-right"></div>
+            <div className="italic mt-5 text-white text-sm">
+              "This is a very interesting feedback from the client"
+            </div>
+          </div>
+          <div className="h-full w-1/2 md:w-1/6 flex justify-center items-center flex-col bg-primary/70 rounded-md shadow-lg p-5">
+            <div className="bg-profile rounded-full h-20 w-20 bg-cover bg-right"></div>
+            <div className="italic mt-5 text-white text-sm">
+              "This is a very interesting feedback from the client"
+            </div>
+          </div>
+          <div className="h-full w-1/2 md:w-1/6 flex justify-center items-center flex-col bg-primary/70 rounded-md shadow-lg p-5">
+            <div className="bg-profile rounded-full h-20 w-20 bg-cover bg-right"></div>
+            <div className="italic mt-5 text-white text-sm">
+              "This is a very interesting feedback from the client"
+            </div>
+          </div>
+          <div className="h-full w-1/2 md:w-1/6 flex justify-center items-center flex-col bg-primary/70 rounded-md shadow-lg p-5">
+            <div className="bg-profile rounded-full h-20 w-20 bg-cover bg-right"></div>
+            <div className="italic mt-5 text-white text-sm">
+              "This is a very interesting feedback from the client"
+            </div>
+          </div>
+          <div className="h-full w-1/2 md:w-1/6 flex justify-center items-center flex-col bg-primary/70 rounded-md shadow-lg p-5">
+            <div className="bg-profile rounded-full h-20 w-20 bg-cover bg-right"></div>
+            <div className="italic mt-5 text-white text-sm">
+              "This is a very interesting feedback from the client"
+            </div>
+          </div>
+          <div className="h-full w-1/2 md:w-1/6 flex justify-center items-center flex-col bg-primary/70 rounded-md shadow-lg p-5">
+            <div className="bg-profile rounded-full h-20 w-20 bg-cover bg-right"></div>
+            <div className="italic mt-5 text-white text-sm">
+              "This is a very interesting feedback from the client"
+            </div>
+          </div>
+          <div className="h-full w-1/2 md:w-1/6 flex justify-center items-center flex-col bg-primary/70 rounded-md shadow-lg p-5">
+            <div className="bg-profile rounded-full h-20 w-20 bg-cover bg-right"></div>
+            <div className="italic mt-5 text-white text-sm">
+              "This is a very interesting feedback from the client"
+            </div>
+          </div>
+          <div className="h-full w-1/2 md:w-1/6 flex justify-center items-center flex-col bg-primary/70 rounded-md shadow-lg p-5">
+            <div className="bg-profile rounded-full h-20 w-20 bg-cover bg-right"></div>
+            <div className="italic mt-5 text-white text-sm">
+              "This is a very interesting feedback from the client"
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2b323f",
+        secondary: "#f6f7f9",
+      },
+      backgroundImage: {
+        "prop-mgmt":
+          "url('https://images.unsplash.com/photo-1606836559739-7b1d9fbf8a6e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+        "book-keeping":
+          "url('https://images.unsplash.com/photo-1526628953301-3e589a6a8b74?q=80&w=2006&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+        "graphic-design":
+          "url('https://images.unsplash.com/photo-1608699565448-9492aaf3c708?q=80&w=2151&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+        profile:
+          "url('https://images.unsplash.com/photo-1568602471122-7832951cc4c5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+      },
+    },
   },
   plugins: [],
 };
